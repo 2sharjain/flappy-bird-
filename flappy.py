80a20b513d90118ac13f79b51c789df7418eed9f
Naman Arora <36225548+palindrome69@users.noreply.github.com>
2018-03-17 19:35:34 +0530

    Update flappy.py

--- flappy.py
+++ flappy.py
@@ -4,13 +4,16 @@ import time
    
 def gameOver(score):
     canvas.destroy()
-    canvas1=Canvas(root,width = 400,height = 500,bg = "WHITE",bd = 0, highlightthickness = 0)
-    canvas1.create_text(200,200,text="GAME OVER",font=("Comic San MS",50,"bold"))
-    canvas1.create_text(200,250,text=("YOUR SCORE IS : "+str(score)),font=("Comic San MS",20,"bold"))
+    canvas1=Canvas(root,width = 400,height = 500,bg = "BLACK",bd = 0, highlightthickness = 0)
+    canvas1.create_text(200,200,text="GAME OVER",fill="WHITE",font=("Comic San MS",50,"bold"))
+    canvas1.create_text(200,250,text=("YOUR SCORE IS : "+str(score)),fill="WHITE",font=("Comic San MS",20,"bold"))
+    ball1=Ball(canvas1)
     canvas1.pack()
     root.update()
     root.mainloop()
-class ball:
+
+    
+class Ball:
     def __init__(self,canvas):
         self.i = True
         self.x=0
@@ -30,6 +33,8 @@ class ball:
     def bounce(self,event):
         if (self.i== True) :
           self.canvas.move(self.id,0,-40)      
+
+
         
 class Rectangle:
     def __init__(self,canvas,x,z,w):
@@ -41,8 +46,6 @@ class Rectangle:
         self.id2 = self.canvas.create_rectangle(self.x,500,self.x+60,self.w,fill = "WHITE")
         
     def move(self):
-        coords = self.canvas.coords(self.id1)
-        coords = self.canvas.coords(self.id2)
         self.canvas.move(self.id1,-2,0)
         self.canvas.move(self.id2,-2,0)
     def isHit(self):
@@ -52,16 +55,22 @@ class Rectangle:
         p = canvas.coords(ball.id)
         if p[0]-coords1[0]<= 60 and p[0]-coords1[0]>=-60 and p[0]+30>=coords1[0]: #if it hits the rectangles
             if p[1]+30 >= coords2[1] or p[3]-30<=coords1[3]:
+                ball.i=False
                 return True
+
+
+
+            
 def gameStart(m):
  a=0
  l=0
  arr=[]
  j=0
  while m:
+
     x = random.randrange(370,400)
     z = random.randrange(110,180)
-    w = random.randrange(150,450)
+    w = random.randrange(250,450)
     if a%110==0:
         arr.append(Rectangle(canvas,x,w-z,w)) #rectangle objects stored in the array
         
@@ -70,7 +79,7 @@ def gameStart(m):
             if(arr[i].isHit()):
               m=0
     if arr[j].canvas.coords(arr[0].id1)[2]<0:
-         del arr[j]
+         arr.pop(j)
          print("deleted")
          l=l+1
     canvas.bind("<Button-1>",ball.bounce)
@@ -83,14 +92,16 @@ def gameStart(m):
 
 
 
+
+
 root = Tk()
 root.title("ALPHA")
 root.resizable(0,0)
-#root.wm_attributes("-topmost",1)
+root.wm_attributes("-topmost",1)
 canvas = Canvas(root,width = 400,height = 500,bg = "BLACK",bd = 0, highlightthickness = 0)
 canvas.pack()
 root.update()    
-ball = ball(canvas)
+ball = Ball(canvas)
 score=gameStart(m=1)
-time.sleep(0.2)
+time.sleep(0.3)
 gameOver(score)
