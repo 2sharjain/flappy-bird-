c471fc8de02ae7caffb8756917efcbe8255f98cd
Naman Arora <36225548+palindrome69@users.noreply.github.com>
2018-03-21 00:57:32 +0530

    Update flappy.py

--- flappy.py
+++ flappy.py
@@ -1,8 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from tkinter import *
 import random
 import time
 import os
 import json
+
+
+def sortDic(dic, canvas1):
+    values = list(dic.values())
+    values.sort()
+    print values
+    y = 300
+    j = 2
+    while j >= 0:
+        for i in dic:
+            if dic[i] == values[j]:
+                canvas1.create_text(200, y, text=i + ': '
+                                    + str(values[j]), fill='BLACK',
+                                    font=('Comic San MS', 20, 'bold'))
+                y = y + 50
+        j = j - 1
 
 
 def reStart():
@@ -12,7 +30,7 @@ def reStart():
 
 
 def gameOver(score):
-    print (score)
+    print score
     canvas.destroy()
     canvas1 = Canvas(root, width=400, height=500, bg='WHITE')
     canvas1.create_text(200, 200, text='GAME OVER', fill='BLACK',
@@ -26,14 +44,7 @@ def gameOver(score):
 
     def setName():
         name = entry.get()
-        y = 300
-        leaders = leaderboards(name, score)
-
-        for i in leaders:
-            canvas1.create_text(200, y, text=i + ': '
-                                + str(leaders[i]), fill='BLACK',
-                                font=('Comic San MS', 20, 'bold'))
-            y = y + 50
+        sortDic(leaderboards(name, score), canvas1)
         button2.pack_forget()
         entry.pack_forget()
 
@@ -51,7 +62,7 @@ class Ball:
     def __init__(self, canvas):
         self.i = True
         self.x = 0
-        self.y = 1.25
+        self.y = 2
         self.canvas = canvas
         self.id = self.canvas.create_oval(10, 180, 40, 210, fill='RED')
         self.canvas.move(self.id, 30, 0)
@@ -91,35 +102,40 @@ class Rectangle:
                 + 60, 500, fill='WHITE')
 
     def move(self):
-        self.canvas.move(self.id1, -2, 0)
-        self.canvas.move(self.id2, -2, 0)
+        self.canvas.move(self.id1, -2.2, 0)
+        self.canvas.move(self.id2, -2.2, 0)
 
     def isHit(self):
         p = []
-        coords1 = self.canvas.coords(self.id1)  # for the rectangle the coordinates are the end points of the diagonal
+        coords1 = self.canvas.coords(self.id1)
         coords2 = self.canvas.coords(self.id2)
-        p = canvas.coords(ball.id)  # p=[x1,y1,x2,y2]
+        p = canvas.coords(ball.id)
 
         if p[0] - coords1[0] <= 60 and p[0] - coords1[0] >= -60 \
-            and p[2] >= coords1[0]:  # if it hits the rectangles.    #for the oval(ball) the coordinates are as x1  x2
+            and p[2] >= coords1[0]:
 
-            if p[1] + 30 >= coords2[1] or p[1] <= coords1[3]:  #                                            y2
+            if p[1] + 30 >= coords2[1] or p[1] <= coords1[3]:
                 ball.i = False
                 return True
 
 
 def leaderboards(name, score):
-    with open('leaderboards.txt', 'r') as f:
-        leaderboards = json.load(f)
+    try:
+        with open('leaderboards.txt', 'r') as f:
+            leaderboards = json.load(f)
+    except:
+        with open('leaderboards.txt', 'w') as f:
+            leaderboards = {'name1': 3, 'name2': 2, 'name3': 1}
+            json.dump(leaderboards, f)
     score = int(score)
-    print (score)
+    print score
     arr = []
     for i in leaderboards:
         arr.append(leaderboards[i])
     arr.append(score)
 
     arr.sort()
-    print (arr)
+    print arr
     if score > arr[0]:
         for i in leaderboards:
             if arr[0] == leaderboards[i]:
@@ -127,27 +143,26 @@ def leaderboards(name, score):
         leaderboards.pop(m)
         scr = arr
         scr.pop(0)
-        print (scr)
+        print scr
 
         leaderboards[name] = score
 
-    with open('leaderboards.txt', 'w') as f:
-        json.dump(leaderboards, f)
-    print (leaderboards)
+    print leaderboards
     return leaderboards
 
 
 def gameStart(m, canvas):
-
     a = 0
     l = 0
     arr = []
 
     canvas.bind('<Button-1>', ball.bounce)
+    space = 90
+    x = 380
+    difficulty = 85
     while m:
-        if a % 110 == 0:
-            x = random.randrange(370, 400)
-            z = random.randrange(110, 180)
+        if a % difficulty == 0:
+            z = random.randrange(space, space + 80)
             w = random.randrange(250, 450)
             arr.append(Rectangle(canvas, x, w - z, w))  # new Rectangle object stored in the array
             a = 0
@@ -161,6 +176,9 @@ def gameStart(m, canvas):
         if arr[0].canvas.coords(arr[0].id1)[2] < 0:
             del arr[0]
             l = l + 1
+            if l > 15:
+                space = 70
+                difficulty = 80
         ball.goDown()
         root.update()
         a = a + 1
@@ -178,3 +196,5 @@ score = gameStart(1, canvas)
 time.sleep(0.25)
 
 gameOver(score)
+
+	
