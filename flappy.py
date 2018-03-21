8e67dace84632418afd9542db64e2878061ee00f
Naman Arora <36225548+palindrome69@users.noreply.github.com>
2018-03-21 16:12:31 +0530

    Update flappy.py

--- flappy.py
+++ flappy.py
@@ -1,126 +1,145 @@
+
 from tkinter import *
 import random
 import time
 import os
 import json
 
-def reStart():
-    root.destroy()
-    os.system('python flappy.py')
+'''Class GameOver is the class with methods which make the game over window and the present the high scores'''
+
+class GameOver:
+    def __init__(self,score,canvas): #make the gameover window and displayes the scores.
+        self.score=score
+        self.leaderboards={}   #dictionary containing the high scores.
+        print (score)
+        canvas.destroy()
+        
+        self.canvas1 = Canvas(root, width=400, height=500, bg='WHITE')
     
-def sortedDict(score,name):
- t=1
- try:   
-  with open('leaderboards.txt', 'r') as f:
-        leaderboards = json.load(f)
- except:
-     leaderboards={"3":"name","2":"name","1":"name"}
- scores=list(leaderboards.keys())
- scores=[int(i) for i in scores]
- for i in scores:
-     if score==i:
-         leaderboards[str(score)]=name
-         t=0
- if t!=0:        
-  print(scores)
-  scores.append(score)
-  print(scores)
-  scores.sort()
-  if score>scores[0]:
-    leaderboards.pop(str(scores[0]))   
-  scores.pop(0)
-  scores=[str(i) for i in scores]
-  for i in scores:
-        if str(score)==i:
-          leaderboards[i]=name
-           
- with open('leaderboards.txt', 'w') as f:
-        json.dump(leaderboards, f)
- print (leaderboards)
- return leaderboards        
-
-    
-
-
-def gameOver(score):
-    print (score)
-    canvas.destroy()
-    canvas1 = Canvas(root, width=400, height=500, bg='WHITE')
-    txt1 = canvas1.create_text(200, 200, text='GAME OVER', fill='BLACK',
+        self.canvas1.create_text(200, 200, text='GAME OVER', fill='BLACK',
                         font=('Comic San MS', 50, 'bold'))
-    txt = canvas1.create_text(200, 250, text='YOUR SCORE IS :'
+    
+        self.canvas1.create_text(200, 250, text='YOUR SCORE IS :'
                               + str(score), fill='BLACK',
-                              font=('Comic San MS', 25, 'bold'))
-    canvas1.pack()
-    entry = Entry(root)
-    entry.pack()
+                              font=('Comic San MS', 25,))
+        self.canvas1.pack()
+        self.entry = Entry(root)
+        self.entry.pack()
+        self.button1 = Button(root, text='ENTER', command=self.displayScores)
+        self.button1.pack()
+        self.button2 = Button(root, text='RESTART', command=self.reStart)
+        self.button2.pack()
 
-    def setName():
-        name = entry.get()
+        root.update()
+        root.mainloop()
+        
+
+    def displayScores(self):
+        name = self.entry.get()
         y = 300
-        leaders = sortedDict(score,name)
-        scores=list(leaders.keys())
-        scores=[int(i) for i in scores]
-        scores=sorted(scores,reverse=True)
+        leaders = self.leaderBoard(name)
+        scores=list(leaders.keys())  #scores as strings
+        scores=[int(i) for i in scores] #scores as integers
+        scores=sorted(scores,reverse=True) #reverse sorting 
         for i in scores:
-            canvas1.create_text(200, y, text=str(i) + ': '
+            self.canvas1.create_text(200, y, text=str(i) + ': '       #displaying scores on the canvas in decreasing order
                                 + str(leaders[str(i)]), fill='BLACK',
                                 font=('Comic San MS', 20, 'bold'))
             y = y + 50
-        button1.pack_forget()
-        canvas1.delete(txt)
-        entry.pack_forget()
+        self.button1.pack_forget() #removes the enter button once the leaderboard is displayed.
+        self.entry.pack_forget()   #removes the text field once the leaderboard is displayed.
+        
 
-    button1 = Button(root, text='ENTER', command=setName)
-    button1.pack()
-    button2 = Button(root, text='RESTART', command=reStart)
-    button2.pack()
+    def reStart(self):   #restarts the game
+        root.destroy()
+        os.system("python ll.py")
 
-    root.update()
-    root.mainloop()
+        
+    def leaderBoard(self,name):   #stores the scores and the corresponding names as a dictionary in a json file.
+       t=1                        # t represents if the score is equal to any scores the leaderboars. if t==0, score is already in the leaderboard.
+       try:   
+         with open('leaderboards.txt', 'r') as f:
+           leaderboards = json.load(f)
+       except:
+         leaderboards={"3":"name","2":"name","1":"name"}  #the scores are the keys and the names are the values.
+       scores=list(leaderboards.keys())
+       scores=[int(i) for i in scores]        #list of scores in type int.
+       
+       for i in scores:                       #checks if the score is already in the current leaderboard.
+         if self.score==i:
+          leaderboards[str(self.score)]=name
+          t=0
+          
+       if t!=0:                               #if the score is not equal to any scores.
+         print(scores) 
+         scores.append(self.score)            
+         print(scores)
+         
+         scores.sort()
+         
+         if self.score>scores[0]:
+           leaderboards.pop(str(scores[0]))
+           
+         scores.pop(0)                       #deletes the first element of the appended scores list.
+         scores=[str(i) for i in scores]
+         
+       for i in scores:                      #if the score is not deleted(it is high enough to be on leaderboard or the scores list)
+         if str(self.score)==i:
+           leaderboards[i]=name              #the score is stored in the dictionary.
+           
+       with open('leaderboards.txt', 'w') as f:
+        json.dump(leaderboards, f)
+        
+       print (leaderboards)
+       return leaderboards     
 
+'''ball object belongs to the class Ball and it defines the actions and parameters of the ball(bird) which is used in the game'''
 
 class Ball:
 
-    def __init__(self, canvas):
-        self.i = True
-        self.x = 0
-        self.gravity = 2
+    def __init__(self, canvas): #makes the ball appear on the canvas
+        self.willMove = True    #when ball hits the rectangle this parameter will be False
+        self.x = 0             #x co-ordinate speed of the ball. will be always zero.
+        self.gravity = 2       #the gravity checks the rate of falling down.  
         self.canvas = canvas
-        self.id = self.canvas.create_oval(10, 180, 40, 210, fill='RED')
-        self.canvas.move(self.id, 30, 0)
+        self.id = self.canvas.create_oval(10, 180, 40, 210, fill='RED') #tkinter id of the oval(ball)
+        self.canvas.move(self.id, 30, 0)   #places the ball in the position during the starting of the game.
 
-    def goDown(self):
+    def goDown(self):     #ball falls down. it is called in the while loop in the function gameStart
 
-        coords = self.canvas.coords(self.id)
-        if self.i == True:
+        coords = self.canvas.coords(self.id) #the current co-ordinates of the ball.returns a tuple
+        
+        if self.willMove == True:
             self.canvas.move(self.id, self.x, self.gravity)
             coords = self.canvas.coords(self.id)
 
-        if coords[3] > 500:
+        if coords[3] > 500:   #if it hits the bottom of screen
             self.gravity = 0
-            self.i = False
+            self.willMove = False
 
-    def bounce(self, event):
-        if self.i == True:
+    def bounce(self, event): #bounces as the mouse button is clicked
+        if self.willMove == True:
             self.canvas.move(self.id, 0, -40)
 
-class Rectangle:
+
+''' class of rectangle objects created in the game through which the ball bounces'''            
+
+class Rectangle:   
 
     def __init__(
         self,
         canvas,
         x,
         z,
-        w,
+        y,
         ):
         self.x = x
-        self.w = w
+        self.y = y
         self.z = z
         self.canvas = canvas
-        self.id1 = self.canvas.create_rectangle(self.x, 0, self.x + 60,
-                self.z, fill='WHITE')
-        self.id2 = self.canvas.create_rectangle(self.x, self.w, self.x
+        self.id1 = self.canvas.create_rectangle(self.x, 0, self.x + 60,  #the rectangle object consists of two rectangles created in the same x 
+                self.z, fill='WHITE')                                    # with the space between them equal to z and the y coord of the lower rect as y
+        self.id2 = self.canvas.create_rectangle(self.x, self.y, self.x   #breadth of the rectangle is 60
                 + 60, 500, fill='WHITE')
 
     def move(self):
@@ -129,48 +148,52 @@ class Rectangle:
 
     def isHit(self):
         p = []
-        coords1 = self.canvas.coords(self.id1)  # for the rectangle the coordinates are the end points of the diagonal
-        coords2 = self.canvas.coords(self.id2)
-        p = canvas.coords(ball.id)  # p=[x1,y1,x2,y2]
+        coords1 = self.canvas.coords(self.id1)  #coordinates of rectangle(upper)
+        coords2 = self.canvas.coords(self.id2)  #coordinates of rectangle(lower)
+        p = canvas.coords(ball.id)  #co-ordinates of the ball
 
-        if p[0] - coords1[0] <= 60 and p[0] - coords1[0] >= -60 \
-            and p[2] >= coords1[0]:  # if it hits the rectangles.    #for the oval(ball) the coordinates are as x1  x2
+        if p[0] - coords1[0] <= 60 and p[0] - coords1[0] >= -60 and p[2] >= coords1[0]:  # if the ball hits the rectangles.     
 
-            if p[1] + 30 >= coords2[1] or p[1] <= coords1[3]:  #                                            y2
-                ball.i = False
+            if p[1] + 30 >= coords2[1] or p[1] <= coords1[3]: 
+                ball.willMove = False
                 return True
 
-def gameStart(m, canvas):
+def gameStart(game, canvas):
 
     a = 0
     l = 0
-    arr = []
+    rectObjects = []
     difficulty=85
 
-    while m:
+    while game:
         ball.goDown()
-        for i in arr:
-            i.move()        
+        
+        for i in rectObjects:
+            i.move()
+            
         if a % difficulty == 0:
             z = random.randrange(110, 180)
             w = random.randrange(200, 450)
-            arr.append(Rectangle(canvas, 380, w - z, w))  # new Rectangle object stored in the array
+            rectObjects.append(Rectangle(canvas, 380, w - z, w))  # new Rectangle object stored in the array
             a = 0
         
-        if arr[0].isHit():
-                m = 0
-                for i in arr:
+        if rectObjects[0].isHit():
+                game = False
+                for i in rectObjects:
                     del i
+                    
         canvas.bind('<Button-1>', ball.bounce)
-        if arr[0].canvas.coords(arr[0].id1)[2] < 0:
-            canvas.delete(arr[0].id1)
-            canvas.delete(arr[0].id2)
-            del arr[0]
+        
+        if rectObjects[0].canvas.coords(rectObjects[0].id1)[2] < 0:
+            canvas.delete(rectObjects[0].id1)
+            canvas.delete(rectObjects[0].id2)
+            del rectObjects[0]
             l = l + 1
             if l>10:
                 difficulty=80
             if l>20:
                 difficulty=75
+                
         root.update()
         a = a + 1
         time.sleep(0.01)
@@ -183,7 +206,6 @@ root.resizable(0, 0)
 canvas = Canvas(root, width=400, height=500, bg='ORANGE')
 canvas.pack()
 ball = Ball(canvas)
-score = gameStart(1, canvas)
+score = gameStart(True, canvas)
 time.sleep(0.25)
-
-gameOver(score)
+gameOver=GameOver(score,canvas)
